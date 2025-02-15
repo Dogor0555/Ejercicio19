fa13dd5ff57a92452c0bc94fa40a501f29670894
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:32:32 -0600

    Ejercicio 19

--- Ejercicio19/Program.cs
+++ Ejercicio19/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    // Función que recibe un número entero y determina si es par o impar
+    static void DeterminarParImpar(int numero)
+    {
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"{numero} es un número par.");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} es un número impar.");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Solicitar un número al usuario
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Llamar a la función para determinar si el número es par o impar
+        DeterminarParImpar(numero);
+    }
+}
